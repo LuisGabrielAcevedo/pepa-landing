aa6fa4aa561f1e186c6fd75436fdb9ca5e109f3b
Gabriel Acevedo <u631186@T0003540283.local>
2021-04-01 20:24:04 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -6,13 +6,20 @@ const openEmail = () => {
 }
 
 const init = () => {
-    const now = new Date().valueOf();
-    setTimeout(function () {
-        if (new Date().valueOf() - now > 100) return;
-        // window.location = "https://itunes.apple.com/appdir";
-    }, 25);
-    window.location = "ar.com.personalpay://linkingScreenRoute/10";
+    
+    try {
+        const now = new Date().valueOf();
+        setTimeout(function () {
+            if (new Date().valueOf() - now > 100) return;
+            console.log('redirect')
+            // window.location = "https://itunes.apple.com/appdir";
+        }, 25);
+        window.location = "ar.com.personalpay://linkingScreenRoute/10";
+    }
+    catch(err) {
+        console.log(err)
+    }
 }
+console.log(window.location.host)
 
-
-init();
+// init();
