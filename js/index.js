25ace6244b29d2c5ba5609878777a6565fbff4cc
Gabriel Acevedo <u631186@T0003540283.local>
2021-04-01 22:51:28 -0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const init = () => {
     try {
+        const queryParams = new URLSearchParams(window.location.search);
+        const useCase = queryParams.get('usecase');
+        const token = queryParams.get('verification_code');
+		const username = queryParams.get('login_hint');
         const now = new Date().valueOf();
         setTimeout(function () {
             if (new Date().valueOf() - now > 100) return;
             window.location.href = "https://luisgabrielacevedo.github.io/pepa-landing/mensajes.html";
         }, 25);
-        window.location = "ar.com.personalpay://linkingScreenRoute/10";
+        window.location = `ar.com.personalpay://linkingScreenRoute/${useCase}/${token}/${username}`;
     }
     catch(err) {
         console.log(err)
