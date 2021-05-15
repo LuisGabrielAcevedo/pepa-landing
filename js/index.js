e75a0f0c4d3c898b4df1a81b8a3d87f354adf890
Gabriel Acevedo <u631186@T0003540283.local>
2021-05-14 22:56:00 -0300

    add

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
+// const init = () => {
+//     const queryParams = new URLSearchParams(window.location.search);
+//     const useCase = queryParams.get('usecase');
+//     const token = queryParams.get('verification_code');
+// 	const username = queryParams.get('login_hint');
+//     const now = new Date().valueOf();
+//     setTimeout(function () {
+//         window.location.href = "mensajes.html";
+//     }, 100);
+        
+//     if (useCase && token && username) 
+//     window.location = `ar.com.personalpay://linkingScreenRoute/${useCase}/${token}/${username}`;
+// }
+
+
 const init = () => {
     const queryParams = new URLSearchParams(window.location.search);
-    const useCase = queryParams.get('usecase');
-    const token = queryParams.get('verification_code');
-	const username = queryParams.get('login_hint');
-    const now = new Date().valueOf();
+    const route = queryParams.get('route');
     setTimeout(function () {
         window.location.href = "mensajes.html";
     }, 100);
         
     if (useCase && token && username) 
-    window.location = `ar.com.personalpay://linkingScreenRoute/${useCase}/${token}/${username}`;
+    window.location = `ar.com.personalpay://${route}`;
 }
-
 
 init();
